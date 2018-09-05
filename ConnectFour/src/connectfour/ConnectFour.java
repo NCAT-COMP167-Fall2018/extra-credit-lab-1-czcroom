7fd4b96c800fe61554519e09dac87946a1e287b3
Clayvon Croom <clayvon.croom42@outlook.com>
2018-09-05 15:18:04 -0400

    level-1-complete

---
+++ ConnectFour/src/connectfour/ConnectFour.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connectfour;
+
+/**
+ *
+ * @author clayvonc
+ */
+public class ConnectFour {
+    private static char[][] gameBoard;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+    private static char[][] createBoard(char startingCharacter){
+        return null;
+    }
+    
+    private static void printBoard(){
+        
+    }
+    
+}
